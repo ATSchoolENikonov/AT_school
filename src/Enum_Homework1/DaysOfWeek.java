cc299103b1c2f13f4fb28cbb68a115d8327c5b3d
Nikonov Egor <methshest@mail.ru>
2022-12-28 22:09:19 +0600

    Enum Home with enum Получилось некрасиво и громоздко, но пока так,по другому придумать нужно побольше изучить

--- src/Enum_Homework1/DaysOfWeek.java
+++ src/Enum_Homework1/DaysOfWeek.java
@@ -1,13 +1,13 @@
 package Enum_Homework1;
 
 public class DaysOfWeek {
-    public static final String MONDAY = "Понедельник";
-    public static final String TUESDAY = "Вторник";
-    public static final String WEDNESDAY = "Среда";
-    public static final String THURSDAY = "Четверг";
-    public static final String FRIDAY = "Пятница";
-    public static final String SATURDAY = "Суббота";
-    public static final String SUNDAY = "Воскресенье";
+    public static final String MONDAY="Понедельник";
+    public static final String TUESDAY="Вторник";
+    public static final String WEDNESDAY="Среда";
+    public static final String THURSDAY ="Четверг";
+    public static final String FRIDAY="Пятница";
+    public static final String SATURDAY="Суббота";
+    public static final String SUNDAY="Воскресенье";
 
 
 }
