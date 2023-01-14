d1159f9e141b0f520f42affecde0bf1cc4bcaddc
methshest <gr4v3000@mail.ru>
2023-01-14 18:05:03 +0600

    Привел к наносекундам замер времени работы ДОбавил более внятные выводы, чтобы было легче понимать

--- Fix/src/Homework.java
+++ Fix/src/Homework.java
@@ -8,7 +8,7 @@ import java.util.GregorianCalendar;
 
 public class Homework {
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
         Date date = new Date(91, 8, 20);
         Date nowDate = new Date();
         long diffDate = Math.abs(nowDate.getTime() - date.getTime());
@@ -17,9 +17,9 @@ public class Homework {
         System.out.println(new SimpleDateFormat("GG-dd-mm-w-yyyy").format(date));
         System.out.println(new SimpleDateFormat("GG-dd-MM-u-yyyy").format(date));
         System.out.println(new SimpleDateFormat("GG-dd-W-MM-E").format(date));
-        System.out.println(diffDate);
-        long dateRun = System.currentTimeMillis();
-        System.out.println("Date running: " + (dateRun - start));
+        System.out.println("Difference: " + diffDate + " Milliseconds");
+        long dateRun = System.nanoTime();
+        System.out.println("Date running: " + (dateRun - start) + " Nanoseconds");
 
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
@@ -36,10 +36,10 @@ public class Homework {
         LocalDate nowLocalDate = LocalDate.now();
         long diffLocalDate = Math.abs(nowLocalDate.toEpochDay() - localDate.toEpochDay());
         Period period = Period.between(localDate, nowLocalDate);//добавил период чисто для пробы
-        System.out.println(diffLocalDate);
+        System.out.println("Difference: " + diffLocalDate + " Days");
         System.out.println(period);
-        long localDateRun = System.currentTimeMillis();
-        System.out.println("LocalDate running: " + (localDateRun - dateRun));
+        long localDateRun = System.nanoTime();
+        System.out.println("LocalDate running: " + (localDateRun - dateRun) + " Nanoseconds");
 
         LocalDateTime localDateTime = LocalDateTime.of(1991, 9, 20, 14, 33);
         System.out.println("Формат 1: " + formatter.format(localDateTime));
@@ -49,9 +49,9 @@ public class Homework {
         System.out.println("Формат 5: " + formatter4.format(localDateTime));
         LocalDateTime nowLocalDateTime = LocalDateTime.now();
         long diffLocalDateTime = Math.abs(localDateTime.toLocalDate().toEpochDay() - nowLocalDateTime.toLocalDate().toEpochDay());
-        System.out.println(diffLocalDateTime);
+        System.out.println("Difference: " + diffLocalDateTime + " Days");
         long localDateTimeRun = System.nanoTime();
-        System.out.println("LocalDateTime running: " + (localDateTimeRun - localDateRun));
+        System.out.println("LocalDateTime running: " + (localDateTimeRun - localDateRun) + " Nanoseconds");
 
 
         ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.of("Asia/Kolkata"));
@@ -62,9 +62,9 @@ public class Homework {
         System.out.println("Формат 5: " + formatter4.format(zonedDateTime));
         ZonedDateTime nowZonedDateTime = ZonedDateTime.of(nowLocalDateTime, ZoneId.of("Asia/Kolkata"));
         long diffZonedDateTime = Math.abs(zonedDateTime.toLocalDate().toEpochDay() - nowZonedDateTime.toLocalDate().toEpochDay());
-        System.out.println(diffZonedDateTime);
+        System.out.println("Difference: " + diffZonedDateTime + " Days");
         long zonedDateTimeRun = System.nanoTime();
-        System.out.println("ZonedDateTime running: " + (zonedDateTimeRun - localDateTimeRun));
+        System.out.println("ZonedDateTime running: " + (zonedDateTimeRun - localDateTimeRun) + " Nanoseconds");
 
         GregorianCalendar gregorianCalendar = new GregorianCalendar(1991, 8, 20);
         Calendar nowGregorianCalendar = GregorianCalendar.getInstance();
@@ -75,10 +75,10 @@ public class Homework {
         System.out.println(gregorianCalendar.toZonedDateTime().format(formatter2));
         System.out.println(gregorianCalendar.toZonedDateTime().format(formatter3));
         System.out.println(gregorianCalendar.toZonedDateTime().format(formatter4));
-        System.out.println(diffCalendar);
+        System.out.println("Difference: " + diffCalendar + " Seconds");
         long calendarRun = System.nanoTime();
-        System.out.println("GregorianCalendar running: " + (calendarRun - zonedDateTimeRun));
-        long end = System.currentTimeMillis();
-        System.out.println("Overall : " + (end - start));
+        System.out.println("GregorianCalendar running: " + (calendarRun - zonedDateTimeRun) + " Nanoseconds");
+        long end = System.nanoTime();
+        System.out.println("Overall : " + (end - start) + " Nanoseconds");
     }
 }
