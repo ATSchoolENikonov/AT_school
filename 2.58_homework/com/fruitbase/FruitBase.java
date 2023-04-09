4d2874dea3645d7908c958f4d80ed6b8af80e27b
methshest <gr4v3000@mail.ru>
2023-04-09 19:47:30 +0600

    3.10 Второе задание доделки

--- 2.58_homework/com/fruitbase/FruitBase.java
+++ 2.58_homework/com/fruitbase/FruitBase.java
@@ -29,10 +29,16 @@ public class FruitBase implements Serializable{
         System.out.println("Каталог эскпортирован");
     }
 
-    public void importCatalogue() throws IOException, ClassNotFoundException {
-        FileInputStream inputStream = new FileInputStream("C:\\Fruitbase\\HomeWork\\home.ser");
+    public void importCatalogue() {
+        try (FileInputStream inputStream = new FileInputStream("C:\\Fruitbase\\HomeWork\\home.ser")) {
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
             this.catal = (FruitCatalogue) objectInputStream.readObject();
+            objectInputStream.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
         System.out.println("Каталог импортирован");
     }
 }
--- 2.58_homework/com/fruitbase/Simulation.java
+++ 2.58_homework/com/fruitbase/Simulation.java
@@ -3,27 +3,27 @@ package com.fruitbase;
 import com.fruitbase.Customers.Customer;
 import com.fruitbase.Customers.FreshCustomer;
 import com.fruitbase.Customers.UniqueCustomer;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Simulation {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) {
         FruitBase fr = new FruitBase();
-        if(args[0].equals("-e") || args[0].equals("--export")){
-            fr.exportCatalogue();
+        if (args.length == 0) {
+            System.out.println("No valid order");
+            System.exit(12);
         }
-        if(args[0].equals("-i") || args[0].equals("--import")){
+        if (args[0].equals("-e") || args[0].equals("--export")) {
+            fr.exportCatalogue();
+        } else if (args[0].equals("-i") || args[0].equals("--import")) {
             fr.importCatalogue();
         }
         Customer[] customers = new Customer[]{
                 new FreshCustomer(new ArrayList<>(), "Ivan"),
                 new UniqueCustomer(new ArrayList<>(), "Lexa")
         };
-        if (args.length == 0) {
-            System.out.println("No valid order");
-            System.exit(12);
-        }
         Cargo car = new Cargo();
         fr.takeOrder(args, car);
         System.out.println(car);
