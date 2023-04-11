024aec8f9986f19aca38bf1211e39b210670a6da
methshest <gr4v3000@mail.ru>
2023-04-11 23:39:43 +0600

    3.10 Первое задание Добавил реализацию по умолчанию

--- 3.10_homework/Alchemy.java
+++ 3.10_homework/Alchemy.java
@@ -6,15 +6,15 @@ public class Alchemy {
         List<NatureElement> elems = new ArrayList<>();
         for (int i = 0; i < args.length; i++) {
             elems.add(NatureElement.create(args[i]));
-            if(elems.get(i)== null){
+            if (elems.get(i) == null) {
                 System.out.println("Не удалось создать элемент");
                 System.exit(23);
             }
         }
         for (int i = 0; i < elems.size(); i += 2) {
-            if(elems.get(i)==elems.get(elems.size()-1)){
+            if (i == elems.size() - 1) {
                 System.out.println("Для последнего элемента нет пары");
-            }else {
+            } else {
                 elems.get(i).connect(elems.get(i + 1));
             }
         }
