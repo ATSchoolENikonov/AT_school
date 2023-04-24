7021cdbe54a9ab3a1572812a79044eaefd113d51
methshest <gr4v3000@mail.ru>
2023-04-24 23:45:39 +0600

    5.6 2 задание

--- 2.58_homework/com/fruitbase/Simulation.java
+++ 2.58_homework/com/fruitbase/Simulation.java
@@ -4,6 +4,8 @@ import com.fruitbase.Customers.Customer;
 import com.fruitbase.Customers.FreshCustomer;
 import com.fruitbase.Customers.UniqueCustomer;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -14,10 +16,52 @@ public class Simulation {
             System.out.println("No valid order");
             System.exit(12);
         }
-        if (args[0].equals("-e") || args[0].equals("--export")) {
-            fr.exportCatalogue();
-        } else if (args[0].equals("-i") || args[0].equals("--import")) {
-            fr.importCatalogue();
+        if (args[0].contains("-e") || args[0].contains("--export")) {
+            String s;
+            if (args[0].contains("=") && args[0].substring(3).length() != 0) {
+                s = args[0].substring(3);
+            } else {
+                s = FruitBase.getThisIsTheWay();
+            }
+            try {
+                fr.exportCatalogue(s);
+                throw new IOException();
+            } catch (FileNotFoundException e) {
+                System.out.println("Не найден файл " + s);
+                System.exit(11);
+            } catch (IOException e) {
+                System.out.println("Ошибка при экспорте каталога в " + s);
+                System.exit(12);
+            } catch (RuntimeException | Error error) {
+                System.out.println("Непредведенная ошибка при экспорте каталога из файла " + s);
+                System.exit(13);
+            }
+            System.out.println("Каталог эскпортирован");
+        }
+        if (args[0].contains("-i") || args[0].contains("--import")) {
+            String s;
+            if (args[0].contains("=") && args[0].substring(3).length() != 0) {
+                s = args[0].substring(3);
+            } else {
+                s = FruitBase.getThisIsTheWay();
+            }
+            try {
+                fr.importCatalogue(s);
+                throw new UnsupportedClassVersionError();
+            } catch (FileNotFoundException e) {
+                System.out.println("Не найден файл " + s);
+                System.exit(11);
+            } catch (IOException e) {
+                System.out.println("Ошибка при импорта каталога из " + s);
+                System.exit(12);
+            } catch (UnsupportedClassVersionError e) {
+                System.out.println("Невозможно импортировать каталог из файла : неподдерживаемая версия");
+                System.exit(14);
+            } catch (RuntimeException | Error | ClassNotFoundException error) {
+                System.out.println("Непредведенная ошибка при импорте каталога из файла " + s);
+                System.exit(13);
+            }
+            System.out.println("Каталог импортирован");
         }
         Customer[] customers = new Customer[]{
                 new FreshCustomer(new ArrayList<>(), "Ivan"),
