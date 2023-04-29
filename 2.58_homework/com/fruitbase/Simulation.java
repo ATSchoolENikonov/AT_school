3529ff1c07f0a812b13a2b8260828180ba96e940
methshest <gr4v3000@mail.ru>
2023-04-29 19:27:56 +0600

    5.6 Переделал методы в мейне+ переставил и заменил исключение

--- 2.58_homework/com/fruitbase/Simulation.java
+++ 2.58_homework/com/fruitbase/Simulation.java
@@ -6,61 +6,68 @@ import com.fruitbase.Customers.UniqueCustomer;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InvalidClassException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Simulation {
+    String extractPath(String argument) {
+        String s;
+        if (argument.contains("=") && argument.substring(3).length() != 0) {
+            s = argument.substring(3);
+        } else {
+            s = FruitBase.getThisIsTheWay();
+            System.out.println("Не указан путь до файла... " + "\nЗапись/чтение по дефолтному путь...");
+        }
+        return s;
+    }
+
+    void tryExport(FruitBase base, String path) {
+        try {
+            base.exportCatalogue(path);
+        } catch (FileNotFoundException e) {
+            System.out.println("Не найден файл " + path);
+            System.exit(11);
+        } catch (IOException e) {
+            System.out.println("Ошибка при экспорте каталога в " + path);
+            System.exit(12);
+        } catch (RuntimeException | Error error) {
+            System.out.println("Непредведенная ошибка при экспорте каталога из файла " + path);
+            System.exit(13);
+        }
+    }
+
+    void tryImport(FruitBase base, String path) {
+        try {
+            base.importCatalogue(path);
+        } catch (FileNotFoundException e) {
+            System.out.println("Не найден файл " + path);
+            System.exit(11);
+        } catch (InvalidClassException e) {
+            System.out.println("Невозможно импортировать каталог из файла : неподдерживаемая версия");
+            System.exit(14);
+        } catch (IOException e) {
+            System.out.println("Ошибка при импорта каталога из " + path);
+            System.exit(12);
+        } catch (RuntimeException | Error | ClassNotFoundException error) {
+            System.out.println("Непредведенная ошибка при импорте каталога из файла " + path);
+            System.exit(13);
+        }
+    }
+
     public static void main(String[] args) {
         FruitBase fr = new FruitBase();
+        Simulation sim = new Simulation();
         if (args.length == 0) {
             System.out.println("No valid order");
             System.exit(12);
         }
         if (args[0].contains("-e") || args[0].contains("--export")) {
-            String s;
-            if (args[0].contains("=") && args[0].substring(3).length() != 0) {
-                s = args[0].substring(3);
-            } else {
-                System.out.println("Каталог будет экспортирован по дефолтному пути");
-                s = FruitBase.getThisIsTheWay();
-            }
-            try {
-                fr.exportCatalogue(s);
-            } catch (FileNotFoundException e) {
-                System.out.println("Не найден файл " + s);
-                System.exit(11);
-            } catch (IOException e) {
-                System.out.println("Ошибка при экспорте каталога в " + s);
-                System.exit(12);
-            } catch (RuntimeException | Error error) {
-                System.out.println("Непредведенная ошибка при экспорте каталога из файла " + s);
-                System.exit(13);
-            }
+            sim.tryExport(fr, sim.extractPath(args[0]));
             System.out.println("Каталог эскпортирован");
         }
         if (args[0].contains("-i") || args[0].contains("--import")) {
-            String s;
-            if (args[0].contains("=") && args[0].substring(3).length() != 0) {
-                s = args[0].substring(3);
-            } else {
-                System.out.println("Каталог будет импортирован из дефолтного пути");
-                s = FruitBase.getThisIsTheWay();
-            }
-            try {
-                fr.importCatalogue(s);
-            } catch (FileNotFoundException e) {
-                System.out.println("Не найден файл " + s);
-                System.exit(11);
-            } catch (IOException e) {
-                System.out.println("Ошибка при импорта каталога из " + s);
-                System.exit(12);
-            } catch (UnsupportedClassVersionError e) {
-                System.out.println("Невозможно импортировать каталог из файла : неподдерживаемая версия");
-                System.exit(14);
-            } catch (RuntimeException | Error | ClassNotFoundException error) {
-                System.out.println("Непредведенная ошибка при импорте каталога из файла " + s);
-                System.exit(13);
-            }
+            sim.tryImport(fr, sim.extractPath(args[0]));
             System.out.println("Каталог импортирован");
         }
         Customer[] customers = new Customer[]{
