c38dc76e8f30adbcb38b8e076cb4bcd37778dbd2
methshest <gr4v3000@mail.ru>
2023-06-12 02:36:53 +0600

    7.9 Вроде переделал, но не уверен, в каком формате должен быть написан первый вариант, должно ли в одну строчку foreach идти или лучше вот так по-строчно

--- home_7.9/Task_1/Main.java
+++ home_7.9/Task_1/Main.java
@@ -2,14 +2,15 @@ package Task_1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
-public class Main{
+public class Main {
 
-     static<T extends Plant>  void makeSalad(Basket<T> baskettt) {
-        System.out.println("```" + "\nГотовим салатик: " );
+    static <T extends Plant> void makeSalad(Basket<T> baskettt) {
+        System.out.println("```" + "\nГотовим салатик: ");
         System.out.print("Режем ");
-        for (T item: baskettt) {
+        for (T item : baskettt) {
             System.out.print(item + " ");
         }
         System.out.println("\nУгощайся!\n" +
@@ -17,19 +18,26 @@ public class Main{
     }
 
     public static void main(String[] args) {
-        List<Plant> plants=new ArrayList<>();
-        List<Fruit> multifruit=new ArrayList<>();
+        List<Plant> plants = new ArrayList<>();
+        List<Fruit> multifruit = new ArrayList<>();
         Apples apples = new Apples();
         Banana banana = new Banana();
         Tomato tomato = new Tomato();
         Cucumber cucumber = new Cucumber();
-        plants.add(cucumber);
         plants.add(apples);
         plants.add(tomato);
         plants.add(banana);
-        plants.stream().filter((p)->
-            p instanceof  Fruit)
-                .forEach(p->{multifruit.add((Fruit)p);
-                    System.out.println(p);});
+        plants.add(cucumber);
+//        plants.stream().
+//                filter((p) -> p instanceof Fruit).
+//                forEach(p -> {multifruit.add((Fruit) p);
+//                    System.out.println(p);});
+        plants.stream().
+                filter(p -> p instanceof Fruit).
+                map(p -> (Fruit) p).
+                collect(Collectors.toCollection(()->multifruit)).
+                forEach(System.out::println);
     }
 }
+
+
