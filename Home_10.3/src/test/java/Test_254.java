fb3d085c52e7a71e0ff614c01158fd2960ee7f3f
methshest <gr4v3000@mail.ru>
2023-07-12 22:17:13 +0600

    10.3 Доделал 1 задание

--- Home_10.3/src/test/java/Test_254.java
+++ Home_10.3/src/test/java/Test_254.java
@@ -11,23 +11,23 @@ public class Test_254 {
     @Check_task_254
     @ParameterizedTest
     @MethodSource
-    public void TestCount(int input, int exp) {
-        Assertions.assertEquals(exp, input);
+    public void TestCount(int exp, int index, String str) {
+        Assertions.assertEquals(Task_1.countLetters(str, index), exp);
     }
 
     static Stream<Arguments> TestCount() {
         int a = Task_1.str.indexOf("Ц");
         int b = Task_1.str.indexOf("а");
         int c = Task_1.str.indexOf(".");
-        return Stream.of(Arguments.arguments(0, Task_1.countLetters(Task_1.str, a)),
-                Arguments.arguments(7, Task_1.countLetters(Task_1.str, b)),
-                Arguments.arguments(57, Task_1.countLetters(Task_1.str, c)));
+        return Stream.of(Arguments.arguments(0, a, Task_1.str),
+                Arguments.arguments(7, b, Task_1.str),
+                Arguments.arguments(57, c, Task_1.str));
     }
 
     @ParameterizedTest
     @MethodSource({"checkPath"})
-    public void checkPath(String str, String exp) {
-        Assertions.assertEquals(exp, str);
+    public void checkPath(String exp, String[] str) {
+        Assertions.assertEquals(exp, Task_2.strVivod(str));
     }
 
     static Stream<Arguments> checkPath() {
@@ -36,10 +36,10 @@ public class Test_254 {
         String[] word = {"/"};
         return Stream.of(
                 Arguments.arguments("Путь: /bin/\n" +
-                        "Название файла: java", Task_2.strVivod(word1)),
+                        "Название файла: java", word1),
                 Arguments.arguments("Путь: C:\\Program Files\\Java\\bin\\\n" +
-                        "Название файла: java", Task_2.strVivod(word2)),
-                Arguments.arguments("Введен некорректный путь до файла.", Task_2.strVivod(word)));
+                        "Название файла: java", word2),
+                Arguments.arguments("Введен некорректный путь до файла.", word));
     }
 
 }
